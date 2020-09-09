f2dbd6350935a8ddb27f80df4e23bef329a7c5fb
Samuel Oliveira <samuelloliiveira42@gmail.com>
2020-09-09 14:21:05 -0300

    Estudando array com JS

---
+++ arrays.js
@@ -0,0 +1,17 @@
+/*Array é um conjunto de Dados que pode ser acessado por um índice.
+*Um índice é a posição exata do lugar onde esse dado se encontra. 
+*Array sempre começa do índice 0.
+*/
+
+//Exemplo
+let idadeFamilia = [23,56,21,36,30];
+
+//OBS: Pode colocar qualquer tipo de váriavel dentro de um array
+
+console.log(idadeFamilia[0]);//Passa o índice que deseja exibir do array
+console.log(idadeFamilia.length);/**representa o comprimento(tamanho).
+                                    Nesse exemplo a tamanho é 5. Vai 
+                                    de 0 a 5 o nosso array*/ //
+
+
+//OBS: Aspas e apóstrofo servem para declarar string, case serve pra concatenar
